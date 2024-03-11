9c5bc4a7af98ad1d52958c5b87b7bedeae9a6f15
salmanhaseeb <salmanhaseeb@gmail.com>
2024-03-11 13:36:15 +0500

    login with facebook

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get "welcome/index"
+  root "welcome#index"
 end
